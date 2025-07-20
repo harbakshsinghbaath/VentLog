0fbdfafe49e1de38d4709237131e3d4c25d081f9
harbakshsinghbaath <harbakshsinghbaath@gmail.com>
2025-07-20 21:24:10 +0530

    add header and logo to home page for improved layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,8 +5,12 @@ function Home(){
     return(
         <>
             <MainLayout>
+                <div className='header-home'>
+                <img src = '../src/assets/logo.png' className = "home-logo"/>
                 <h1 className = "home-heading">VentLog</h1>
+                </div>
                 <div className="content-home">
+
                 <h2 className = "intro-para-heading">Let it out. Leave it behind.</h2>
                 <p className="home-para">VentLog is your quiet corner of the internet;  a space to let go, breathe, and be honest with yourself. Write one private or anonymous vent a day, share your thoughts if you’d like, and let them disappear by the end of the day. No pressure, no judgment,  just release.</p>
                 <p className="home-para">Whether you're navigating a tough moment or just need to put your feelings into words, VentLog offers a soft place to land. Every day, every mood, every you.</p>
